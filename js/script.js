12a1fc7345c7b66a80fa6644d5f3f67e45507b06
aisha-99 <123507252+aisha-99@users.noreply.github.com>
2024-04-17 15:39:41 -0400

    innit

--- js/script.js
+++ js/script.js
@@ -1,40 +1,46 @@
-var textInURL = window.location.search;
-var parametersInURL = new URLSearchParams(textInURL);
-var productId = parametersInURL.get('id');
-
-console.log(productId);
 
 var client = contentful.createClient({
     space: '9ivsylmw9p3m',
     accessToken: 'SjUQZDO7xSYc2IeDzAWqx5j0WuwEjMLPCPvt6QCeils'
-});
+  });
+  
+  var productSection = document.querySelector('.more-products-section.home');
+  
+  client.getEntries({
+    content_type: 'assignment3'
+  }).then(function (entries) {
+    entries.items.forEach(function (entry) {
+      var productName = entry.fields.productName;
+      var productImage = entry.fields.productImage.fields.file.url;
+      var productPrice = entry.fields.productPrice;
+      var foodDescription = entry.fields.foodDescription;
+  
+      var productBox = document.createElement('div');
+      productBox.classList.add('product-box');
+  
+      var productImageElem = document.createElement('img');
+      productImageElem.src = productImage;
+      productImageElem.alt = productName;
+  
+      var productInfo = document.createElement('div');
+      productInfo.classList.add('product-info');
+  
+      var productNameElem = document.createElement('h3');
+      productNameElem.textContent = productName;
+  
+      var productPriceElem = document.createElement('p');
+      productPriceElem.textContent = '$' + productPrice;
+  
+      productInfo.appendChild(productNameElem);
+      productInfo.appendChild(productPriceElem);
+  
+      productBox.appendChild(productImageElem);
+      productBox.appendChild(productInfo);
+  
+      productSection.querySelector('.product-row').appendChild(productBox);
+    });
+  }).catch(function (error) {
+    console.log("Error fetching data:", error);
+  });
 
-var productSection = document.querySelector('.product-section');
-var productImage = productSection.querySelector('#productImage'); 
-var productDetails = productSection.querySelector('.product-details');
-
-client.getEntry(productId).then(function (entry) {
-    console.log(entry);
-
-    // Product Name
-    var productName = document.createElement('h2');
-    productName.innerHTML = entry.fields.productName;
-    productDetails.appendChild(productName);
-
-    // Product Image
-    console.log(entry.fields.productImage.fields.file.url);
-    var productImage = document.createElement('img');
-    image.src = entry.fields.productImage.fields.file.url;
-    productImage.appendChild(image); 
-
-    // Cost
-    var cost = document.createElement('p');
-    cost.innerHTML = 'Price: ' + entry.fields.cost;
-    productDetails.appendChild(cost);
-
-    // Description
-    var foodDescription = document.createElement('p');
-    foodDescription.innerHTML = entry.fields.description;
-    productDetails.appendChild(foodDescription);
-});
-
+  
